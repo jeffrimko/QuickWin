a1606db176731a0d89991872401385af891ca702
unknown <jeffrimko@gmail.com>
2016-03-03 15:24:11 -0500

    Updates to build script.

--- app/_Build_App.py
+++ app/_Build_App.py
@@ -20,39 +20,57 @@ import qprompt
 # Function alias.
 call = functools.partial(subprocess.call, shell=True)
 
+# Maintains the build type.
+BUILD = None
+
 ##==============================================================#
 ## SECTION: Function Definitions                                #
 ##==============================================================#
 
-def build(config):
-    build = "release"
-    if not qprompt.ask_yesno("Build release (else debug)?", dft=config['release']):
-        build = "debug"
-    # Build application.
+def kill():
     call("taskkill /f /im quickwin.exe")
+
+def run():
+    global BUILD
+    if None == BUILD:
+        qprompt.alert("[WARNING] Build app first!")
+        return
+    retdir = os.getcwd()
+    os.chdir(BUILD)
+    kill()
+    call("start quickwin.exe")
+    os.chdir(retdir)
+
+def build(config):
+    global BUILD
+    BUILD = "release"
+    if not qprompt.ask_yesno("Build release (else debug)?", dft=config['release']):
+        BUILD = "debug"
+    # Build application.
     call("uic mainwindow.ui >> ui_mainwindow.h")
     call("qmake")
-    if not os.path.exists(build):
-        os.makedir(build)
-    status = call("make %s" % (build))
-    # Copy over Qt DLLs.
-    for dll in config['qt_dlls']:
-        src = os.path.join(config['qt_bin_dir'], dll)
-        shutil.copy(src, build)
-    # Copy over assets.
-    for ast in config['assets']:
-        shutil.copy(ast, build)
-    # Remove unnecessary files from build dir.
-    for f in os.listdir(build):
-        if any([f.endswith(e) for e in config['build_dir_ext_rm']]):
-            os.remove(os.path.join(build, f))
+    if not os.path.exists(BUILD):
+        os.makedir(BUILD)
+    status = call("make %s" % (BUILD))
+    try:
+        # Copy over Qt DLLs.
+        for dll in config['qt_dlls']:
+            src = os.path.join(config['qt_bin_dir'], dll)
+            shutil.copy(src, BUILD)
+        # Copy over assets.
+        for ast in config['assets']:
+            shutil.copy(ast, BUILD)
+        # Remove unnecessary files from build dir.
+        for f in os.listdir(BUILD):
+            if any([f.endswith(e) for e in config['build_dir_ext_rm']]):
+                os.remove(os.path.join(BUILD, f))
+    except:
+        qprompt.alert("File copy failed! Try killing app.")
+        status = 1
     # Run app.
     if status == 0:
         if qprompt.ask_yesno("Run app?", dft=True):
-            retdir = os.getcwd()
-            os.chdir(build)
-            call("start quickwin.exe")
-            os.chdir(retdir)
+            run()
     else:
         qprompt.alert("ERROR: Build failed!")
 
@@ -62,4 +80,10 @@ def build(config):
 
 if __name__ == '__main__':
     config = yaml.load(open("build.yaml").read())
-    build(config)
+    menu = qprompt.Menu()
+    menu.add("b", "build app", build, [config])
+    menu.add("r", "run app", run)
+    menu.add("k", "kill app", kill)
+    menu.add("q", "quit")
+    while "q" != menu.show():
+        pass
